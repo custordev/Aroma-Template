f6a0437c6a8e5d3cde5025b9bc046f38637bc7a6
Ian Sebugenyi <140151921+Iancustor@users.noreply.github.com>
2024-09-03 12:20:55 +0300

    updated the auth in the config

--- config/auth.ts
+++ config/auth.ts
@@ -1,14 +1,12 @@
-import { AuthOptions, NextAuthOptions } from "next-auth";
-// import { PrismaAdapter } from "@auth/prisma-adapter";
+import { NextAuthOptions } from "next-auth";
 import { PrismaAdapter } from "@auth/prisma-adapter";
-
 import GoogleProvider from "next-auth/providers/google";
-import type { Adapter } from "next-auth/adapters";
 import GitHubProvider from "next-auth/providers/github";
 import CredentialsProvider from "next-auth/providers/credentials";
 import { compare } from "bcrypt";
 import { db } from "@/lib/db";
-// more providers at https://next-auth.js.org/providers
+import { Adapter } from "next-auth/adapters";
+
 export const authOptions: NextAuthOptions = {
   adapter: PrismaAdapter(db) as Adapter,
   secret: process.env.NEXTAUTH_SECRET,
@@ -20,13 +18,12 @@ export const authOptions: NextAuthOptions = {
   },
   providers: [
     GitHubProvider({
-      //Checking if the role exista and if not add USER Bydefault
       profile(profile) {
         return {
           id: profile.id.toString(),
           name: profile.name || profile.login,
-          firstName: profile.name?.split("")[0] || "",
-          lastName: profile.name?.split("")[1] || "",
+          firstName: profile.name?.split(" ")[0] || "",
+          lastName: profile.name?.split(" ")[1] || "",
           phone: "",
           image: profile.avatar_url,
           email: profile.email,
@@ -37,7 +34,6 @@ export const authOptions: NextAuthOptions = {
       clientSecret: process.env.GITHUB_CLIENT_SECRET || "",
     }),
     GoogleProvider({
-      //Checking if the role exista and if not add USER Bydefault
       profile(profile) {
         return {
           id: profile.sub,
@@ -60,82 +56,57 @@ export const authOptions: NextAuthOptions = {
         password: { label: "Password", type: "password" },
       },
       async authorize(credentials) {
-        try {
-          console.log(
-            "Authorize function called with credentials:",
-            credentials
-          );
-          // Check if user credentials are Correct
-          if (!credentials?.email || !credentials?.password) {
-            throw { error: "No Inputs Found", status: 401 };
-          }
-          console.log("Pass 1 checked ");
-          //Check if user exists
-          const existingUser = await db.user.findUnique({
-            where: { email: credentials.email },
-          });
-
-          if (!existingUser) {
-            console.log("No user found");
-            throw { error: "No user found", status: 401 };
-          }
-
-          console.log("Pass 2 Checked");
-          console.log(existingUser);
-          let passwordMatch: boolean = false;
-          //Check if Password is correct
-          if (existingUser && existingUser.password) {
-            // if user exists and password exists
-            passwordMatch = await compare(
-              credentials.password,
-              existingUser.password
-            );
-          }
-          if (!passwordMatch) {
-            console.log("Password incorrect");
-            throw { error: "Password Incorrect", status: 401 };
-          }
-          console.log("Pass 3 Checked");
-          const user = {
-            id: existingUser.id,
-            name: existingUser.name,
-            firstName: existingUser.firstName,
-            LastName: existingUser.lastName,
-            image: existingUser.image,
-            email: existingUser.email,
-            role: existingUser.role,
-            phone: existingUser.phone,
-          };
-          //
-          console.log("User Compiled");
-          console.log(user);
-          return user;
-        } catch (error) {
-          console.log("aLL Failed");
-          console.log(error);
-          throw { error: "Something went wrong", status: 401 };
+        if (!credentials?.email || !credentials?.password) {
+          throw new Error("No Inputs Found");
         }
+
+        const existingUser = await db.user.findUnique({
+          where: { email: credentials.email },
+        });
+
+        if (!existingUser || !existingUser.password) {
+          throw new Error("No user found or password missing");
+        }
+
+        const passwordMatch = await compare(
+          credentials.password,
+          existingUser.password
+        );
+
+        if (!passwordMatch) {
+          throw new Error("Password Incorrect");
+        }
+
+        return {
+          id: existingUser.id,
+          name: existingUser.name,
+          firstName: existingUser.firstName,
+          lastName: existingUser.lastName,
+          image: existingUser.image,
+          email: existingUser.email,
+          role: existingUser.role,
+          phone: existingUser.phone,
+        };
       },
     }),
   ],
   callbacks: {
     async jwt({ token, user }) {
-      console.log("JWT Callback", { token, user });
       if (user) {
-        token.id = user.id,
-          token.name = user.name,
-          token.email = user.email,
-        token.role = user.role,
-          token.firstName = user.firstName,
-          token.LastName = user.lastName,
-          token.picture = user.image,
-          token.phone = user.phone;
+        token.id = user.id as string;
+        token.name = user.name;
+        token.email = user.email;
+        token.role = user.role;
+        token.firstName = user.firstName;
+        token.lastName = user.lastName;
+        token.picture = user.image;
+        token.phone = user.phone;
       }
       return token;
     },
     session({ session, token }) {
       if (token && session.user) {
-        session.user.id = token.id;
+        session.user.id = token.id as string;
         session.user.name = token.name;
         session.user.email = token.email;
         session.user.firstName = token.firstName;
