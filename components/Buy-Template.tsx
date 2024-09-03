3abac6153ee4c48ca36cd6345421749748f96d23
Ian Sebugenyi <140151921+Iancustor@users.noreply.github.com>
2024-09-03 11:27:15 +0300

    updated form inputs

--- components/Buy-Template.tsx
+++ components/Buy-Template.tsx
@@ -4,7 +4,8 @@ export function ButtonDemo() {
   return (
     <div className="relative">
       <Link
-        href="#"
+        title="WhatsUp"
+        href="https://wa.me/message/CLAOT4PPP6VDO1"
         className="bg-gray-50 hover:bg-gray-100 py-2 px-4 text-blue-600 font-bold rounded-md fixed right-6 bottom-6 "
       >
         Buy Template
